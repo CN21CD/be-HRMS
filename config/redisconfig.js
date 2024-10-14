4b61f42430ebac1911f85658b8db450ca0c3aa69
vtit6109 <vvt.6109mwm@gmail.com>
2024-10-14 23:02:26 +0700

    set up connect redis

---
+++ config/redisconfig.js
@@ -0,0 +1,24 @@
+const { createClient } = require('redis');
+
+const client = createClient({
+    password: 'u0WIDqmvrneqaQfQlFoTV2smE2vWOL1q',
+    socket: {
+        host: 'redis-12201.c17.us-east-1-4.ec2.redns.redis-cloud.com',
+        port: 12201
+    }
+});
+
+client.on('error', (err) => {
+    console.error('Redis client error:', err);
+});
+
+client.on('end', () => {
+    console.log('Redis client disconnected');
+});
+
+async function redisConnect() {
+    await client.connect();
+    console.log('Redis client connected');
+}
+
+module.exports = { client, redisConnect };

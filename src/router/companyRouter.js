aeb2327e19b2887565ce7db8c95f64be68b97e11
vtit6109 <vvt.6109mwm@gmail.com>
2024-10-16 23:33:22 +0700

    apply authMiddleware

--- src/router/companyRouter.js
+++ src/router/companyRouter.js
@@ -4,8 +4,8 @@ const verifyToken = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.get('/companies',verifyToken.authMiddleware,verifyToken.userMiddleware,companyController.getAllCompanies);
-router.get('/company/:id',verifyToken.userMiddleware,companyController.getCompanyById);
+router.get('/company/:id',verifyToken.authMiddleware,verifyToken.userMiddleware,companyController.getCompanyById);
 // router.post('/company',companyController.addCompany);
-router.put('/company/:id',verifyToken.userMiddleware,companyController.updateCompanyByID); 
+router.put('/company/:id',verifyToken.authMiddleware,verifyToken.userMiddleware,companyController.updateCompanyByID); 
 // router.delete('/company/:id',companyController.deleteCompanyByID);
 module.exports = router;
--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -4,10 +4,10 @@ const verifyToken = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
-router.get('/users', verifyToken.adminMiddleware,userController.getAllUsers);
-router.get('/user/:id', verifyToken.adminMiddleware,userController.getUserById);
-router.post('/user',verifyToken.adminMiddleware, userController.addUser);
-router.put('/user/:id',verifyToken.adminMiddleware, userController.updateUserById);
+router.get('/users',verifyToken.authMiddleware, verifyToken.adminMiddleware,userController.getAllUsers);
+router.get('/user/:id',verifyToken.authMiddleware, verifyToken.adminMiddleware,userController.getUserById);
+router.post('/user',verifyToken.authMiddleware,verifyToken.adminMiddleware, userController.addUser);
+router.put('/user/:id',verifyToken.authMiddleware,verifyToken.adminMiddleware, userController.updateUserById);
 // router.delete('/user/:id', userController.deleteUserById);
 
 module.exports = router;
